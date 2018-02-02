c62e3ed175c294f1ea2cc4cd33346a0460906ba4
toshiki1998 <33944505+toshiki1998@users.noreply.github.com>
2018-02-02 10:58:48 +0900

    Add files via upload

---
+++ MyServer05.java
@@ -0,0 +1,49 @@
+import java.util.Date;
+import java.io.*;
+ import java.net.*;
+ import java.util.*;
+ 
+ class MyServer05{
+ 	public static void main(String[] args) {
+ 		Date date = new Date();
+ 		byte[] buff = new byte[1024];
+ 		ServerSocket servsock = null;
+ 		Socket sock;
+ 		OutputStream out;
+ 		InputStream instr;
+ 		String outstr;
+ 		int i;
+ 		boolean cont = true;
+ 
+ 		try{
+   			
+ 			servsock = new ServerSocket(6000,100);
+ 			while(true){
+ 				sock = servsock.accept();
+ 				System.out.println("接続を確認");
+ 				
+ 				outstr = "date\r\nTI";
+ 				out	= sock.getOutputStream();
+ 				for(i = 0; i<outstr.length(); i++)
+ 					out.write((int)	outstr.charAt(i));
+ 
+ 				out.write('\n');
+     			
+ 				instr = sock.getInputStream();
+ 				while(cont){
+ 					try{
+ 						int n = instr.read(buff);
+ 						System.out.write(buff,	0,	n);
+ 
+ 					}catch(Exception e){
+ 						cont = false;
+ 					}
+ 				}
+ 				
+ 				sock.close();
+ 			}
+ 		}catch(IOException ioe){
+ 			System.exit(1);
+ 		}
+ 	}
+ }
