90cc7d99a46becc0483bec04c37185d7520df934
toshiki1998 <33944505+toshiki1998@users.noreply.github.com>
2018-02-02 10:51:40 +0900

    Add files via upload

---
+++ MyClient03.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+ import java.io.InputStream;
+ import java.net.Socket;
+ import java.io.OutputStream;
+ 
+ public class MyClient03{
+ 	public static void main(String[] args){
+ 		byte[] buff = new byte[1024];
+ 		Socket sock = null;
+ 		InputStream is = null;
+ 		OutputStream os = null;
+ 		boolean	cont	=	true;
+ 		int i;
+ try {
+ 	sock = new Socket("172.16.21.66",6000);
+ 	
+ 	is = sock.getInputStream();
+ 	os = sock.getOutputStream();
+ 
+ 	int n = is.read(buff);
+ 	System.out.write(buff,0,n);
+ 
+ 	String msg = "Hello";
+ 	
+ 	is.close();
+ 
+ 	}	catch (IOException e){
+ 		System.err.println("ネットワークエラー");
+ 		System.exit(1);
+ 		}
+ 	}
+ }
+   
